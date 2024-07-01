d164838a197c0955db1eff2cb5b7894dc4d3164d
Jaeework <jaeework@gmail.com>
2024-07-01 23:31:22 +0900

    #6 - MyBatis 설정 및 테스트
    - root-context.xml 에 mybatis-spring 라이브러리의 클래스를 사용하여 sqlSessionFactory bean 을 등록
    - 테스트코드 작성하여 동작 확인

--- src/test/java/com/tomatoclock/persistence/DataSourceTests.java
+++ src/test/java/com/tomatoclock/persistence/DataSourceTests.java
@@ -2,6 +2,8 @@ package com.tomatoclock.persistence;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,9 @@ public class DataSourceTests {
     @Setter(onMethod_ = { @Autowired })
     private DataSource dataSource;
 
+    @Setter(onMethod_ = { @Autowired })
+    private SqlSessionFactory sqlSessionFactory;
+
     @Test
     public void testConnection() {
 
@@ -28,6 +33,20 @@ public class DataSourceTests {
 
             log.info(con);
 
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testMyBatis() {
+
+        try(SqlSession session = sqlSessionFactory.openSession();
+            Connection con = session.getConnection(); ) {
+
+            log.info(session);
+            log.info(con);
         } catch (Exception e) {
             fail(e.getMessage());
         }

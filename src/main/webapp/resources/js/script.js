f61f34a8ad5eb1513fbd228f4dc5dec848a1e520
Jaeework <jaeework@gmail.com>
2024-07-12 15:37:42 +0900

    #11 - script.js_타이머 기능 구현
    - start, stop, reset 버튼 클릭 시의 타이머 동작 기능 구현
    - 드롭다운 메뉴에서 duration 변경 후, Apply 버튼 클릭 시 동작 구현

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -22,4 +22,67 @@ window.onload = function () {
         document.documentElement.style.setProperty('--color-timer-font', this.value);
     });
 
+
+    // 타이머 기능 구현
+    // variables
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
+    const resetButton = document.getElementById('resetButton');
+
+    let timer;
+    let duration = parseInt(durationSelect.value, 10);
+    let remainingTime = duration * 60;
+
+    function updateDisplay() {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+        minutesDisplay.textContent = String(minutes).padStart(2, '0');
+        secondsDisplay.textContent = String(seconds).padStart(2, '0');
+    }
+
+    function startTimer() {
+        timer = setInterval(function() {
+            if (remainingTime > 0) {
+                remainingTime--;
+                updateDisplay();
+            } else {
+                clearInterval(timer);
+            }
+        }, 1000);
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    function resetTimer() {
+        stopTimer();
+        remainingTime = duration * 60;
+        updateDisplay();
+    }
+
+    startButton.addEventListener('click', function() {
+        startTimer();
+        startButton.style.display = 'none';
+        stopButton.style.display = 'inline-block';
+
+    });
+    stopButton.addEventListener('click', function() {
+        stopTimer();
+        startButton.style.display = 'inline-block';
+        stopButton.style.display = 'none';
+    });
+    resetButton.addEventListener('click', function() {
+        resetTimer();
+        startButton.style.display = 'inline-block';
+        stopButton.style.display = 'none';
+    });
+    // change duration
+    applyDurationButton.addEventListener('click', function () {
+        duration = parseInt(durationSelect.value, 10);
+        resetTimer();
+    });
+
+    // 초기 타이머 설정
+    updateDisplay();
 }
